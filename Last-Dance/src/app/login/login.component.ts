d32b3e3e39b36b9e9db89c8d0fb05ea8f78844ec
David King of Thieves <bisgrovd1@hawkmail.newpaltz.edu>
2018-08-21 05:22:19 -0400

    implemented half of the functionality of the client side

---
+++ Last-Dance/src/app/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/model/user';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  user: User;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
